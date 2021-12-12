15bd4d753aa3ab05cec8eb62a6c2f9a640eae9fb
Alex Hunt <amberhnt1@gmail.com>
2021-12-12 18:15:26 -0500

    Basic HTML layout, stylesheet, and script

---
+++ generator.js
@@ -0,0 +1,10 @@
+// All generator code here
+const RACES = {};
+const CLASSES = {};
+const BACKGROUNDS = {};
+
+// button onclick
+function generateCharacter(){
+    console.log("Generating a character...");
+
+}
