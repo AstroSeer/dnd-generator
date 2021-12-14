9ee0779ee5759c116d9751ab9865761fc5597e3d
AstroSeer <matthewnagy@verizon.net>
2021-12-14 17:23:50 -0500

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,10 +1,13 @@
 // All generator code here
-const RACES = {};
-const CLASSES = {};
+const RACES = ["Aarakocra", "Aasimar", "Bugbear", "Centaur", "Changeling", "Dragonborn", "Dwarf", "Elf", "Fairy", "Firbolg", "Genasi", "Gith", "Gnome", "Goblin", "Goliath", "Half_elf", 
+    "Half_orc", "Halfing", "Harengon", "Hobgoblin", "Human", "Kalashtar", "Kenku", "Kobold", "Leonin", "Lizardfolk", "Loxodon", "Minotaur", "Orc", "Owlin", "Satyr", "Shifter", 
+    "SimicHybrid", "Tabaxi", "Tiefling", "Tortle", "Triton", "Vedalken", "Warforged", "Yuan_Ti_Pureblood"];
+const CLASSES = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer"];
 const BACKGROUNDS = {};
 
 // button onclick
 function generateCharacter(){
     console.log("Generating a character...");
-
+    var randomRaceIndex = Math.floor(Math.random() * RACES.length);
+    console.log(RACES[randomRaceIndex]);
 }
