2c549e265159dd1e88ed4c9cce141e9b58fc9fef
Alex Hunt <amberhnt1@gmail.com>
2021-12-15 17:26:37 -0500

    Framework for generating paragraph

--- generator.js
+++ generator.js
@@ -3,15 +3,40 @@ const RACES = ["Aarakocra", "Aasimar", "Bugbear", "Centaur", "Changeling", "Drag
     "Half orc", "Halfing", "Harengon", "Hobgoblin", "Human", "Kalashtar", "Kenku", "Kobold", "Leonin", "Lizardfolk", "Loxodon", "Minotaur", "Orc", "Owlin", "Satyr", "Shifter", 
     "Simic Hybrid", "Tabaxi", "Tiefling", "Tortle", "Triton", "Vedalken", "Warforged", "Yuan-Ti Pureblood"];
 const CLASSES = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer"];
-const BACKGROUNDS = {};
+const BACKGROUNDS = ["Acolyte", "Caravan Specialist", "Charlatan", "City Watch", ];
+let characterString = ["This character is a ", "[RACE]", " ", "[CLASS]", " with a background as a(n) ", "[BACKGROUND]", "They stand at ",
+    "[HEIGHT]", " and weigh ", "[WEIGHT]", " lbs.", "Something obvious to many is that they ", "[TRAIT].",
+    "They also ", "[QUIRK].", "And they may not be fully aware of it, but they ", "[FLAW].", " Still, deep down, they",
+    "[BOND]."];
 
 // button onclick
 function generateCharacter(){
+    let race, playerclass, background, height, weight, trait, quirk, bond;
+
     console.log("Generating a character...");
-    var randomRaceIndex = Math.floor(Math.random() * RACES.length);
-    console.log(RACES[randomRaceIndex]);
-    document.getElementById('race').innerHTML = RACES[randomRaceIndex];
-    var randomClassIndex = Math.floor(Math.random() * CLASSES.length);
-    console.log(CLASSES[randomClassIndex]);
-    document.getElementById('class').innerHTML = CLASSES[randomClassIndex];
+    let randomRaceIndex = random(RACES.length);
+    // console.log(RACES[randomRaceIndex]);
+    race = RACES[randomRaceIndex];
+
+    let randomClassIndex = random(CLASSES.length);
+    //console.log(CLASSES[randomClassIndex]);
+    playerclass = CLASSES[randomClassIndex];
+
+    characterString[1] = race;
+    characterString[3] = playerclass;
+    //characterString[6] = background;
+    //characterString[8] = height;
+    //characterString[10] = weight;
+    //characterString[13] = trait;
+    //characterString[15] = quirk;
+    //characterString[17] = flaw;
+    //characterString[18] = bond1;
+
+    document.getElementById("results").innerHTML = characterString.join('');
+
+}
+
+function random(max){
+    return Math.floor(Math.random() * max)
+
 }
